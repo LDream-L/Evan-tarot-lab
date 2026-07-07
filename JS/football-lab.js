f6b3575483e1565890330e46e10930f5fffa441a
LDream-L <mengshueh@gmail.com>
2026-07-07 13:46:29 +0800

    更新世足紀錄卡版面快取版本

--- JS/football-lab.js
+++ JS/football-lab.js
@@ -7,7 +7,7 @@
 (function loadFootballLabModules() {
   "use strict";
 
-  const version = "20260706-football-v176-layout-density-c";
+  const version = "20260707-football-v176-record-grid-d";
   const layoutStyleHref = `football-layout-optimizer.css?v=${version}`;
   const finalStyleHref = `football-layout-final.css?v=${version}`;
   const modules = [
